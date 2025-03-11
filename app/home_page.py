4e134b556a5b4499f578a7980a4e68460b0feb13
helenajr <helenajrobinson@gmail.com>
2025-03-11 14:17:34 +0000

    APP: UI improvements text

--- app/home_page.py
+++ app/home_page.py
@@ -33,7 +33,7 @@ waits for admission? (Evidence for a particular management strategy)
 * How does increase demand impact ED waiting times if the bed base stays static? 
 (Evidence for planning)
             
-For some worked examples see the examples page (under construction)
+For some worked examples see the Examples page (under construction)
 
 """)
 
@@ -44,7 +44,7 @@ Patients flow through the system as illustrated in the diagram below.
 
 *  Patients
 enter the system when it is decided that they require admission (Decision To Admit).
-They can be admitted either via the Emergency Department (ED), the Same Day Emergency
+This can be via the Emergency Department (ED), the Same Day Emergency
 Care Department (SDEC) or via another route. Admission demand can be altered for each
 of the 3 routes.
 * Patients wait in ED, SDEC or other until there is a bed available. 
@@ -55,9 +55,37 @@ waiting in ED who are admitted.
 expected to stay in hospital (thereby receiving their treatment in ED) they are discharged from ED.
 * When admitted to a bed a patient remains in that bed and the bed is unavailable
 until they are discharged.
+* The timepoints associated with each simulated patient (Decision To Admit,
+Admission, Discharge) are logged and used to calculate waiting time metrics.
             
 For more detail on how the model works see the 'More Information' page
 """, unsafe_allow_html=True)
 
 st.image("https://raw.githubusercontent.com/Countess-of-Chester-Hospital-NHS-FT/Non-Elective-Flow-Simulation/refs/heads/main/app/img/model_diagram.png")
+
+st.markdown("""
+##### How is the Virtual Hospital different to a real hospital?
+
+The Virtual Hospital is a simplified version of a real hospital. It may differ 
+from a real hospital in the following important ways. In the Virtual Hospital:
+
+* There is no daily or weekly trend in when patients arrive.
+* ED, SDEC and other routes are assumed to be open at all times.
+* Patients can be discharged at any time of day/week.
+* Any patient can occupy any bed.
+* Beds are ready instantly for the next patient.
+* The overall number of beds stays constant through the simulation.
+            
+This means that flow in the Virtual Hospital will always be **better** than flow in
+the real system (queue times in ED will be worse in the real system than in the 
+Virtual Hospital). 
+
+However, the Virtual Hospital can therefore be used to provide a
+best case scenario - if x beds, x demand and x los leads to 10 DTA breaches per day in the Virtual Hospital,
+you would expect **at least** this many DTA breaches per day in the  real system with the
+same parameters. And it can
+still be used to help understand the relationships between beds, demand and los.
+            
+For more detail on model assumptions see the 'More Information' page
+""", unsafe_allow_html=True)
 
