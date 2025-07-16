57c29ba639ef9dd135ab8a1de1435c7cc2385375
helenajr <helenajrobinson@gmail.com>
2025-07-16 13:51:22 +0100

    APP: updated text on home page

--- app/home_page.py
+++ app/home_page.py
@@ -5,15 +5,15 @@ st.set_page_config(
  )
 
 st.title("Non-Elective Flow Virtual Hospital")
-st.header("(work in progress)")
+st.header("(v0.0.0)")
 
 st.markdown("""
 Welcome to the Non-Elective Flow Virtual Hospital. The Virtual Hospital is being
 developed by the Data & Analytics (D&A) team at the Countess of Chester Hospital. 
 The Virtual Hospital is intended to help understand the effect of different 
 management strategies on ED admission delays (DTA waits) using a computer 
-simulation. Please note that this is work in progress and please contact the D&A
- team with any questions or feedback.
+simulation. Please note the limitations below and that this work is at an early stage. Please contact the D&A
+team with any questions or feedback, or raise an issue via GitHub.
 """)
 
 st.markdown("""
@@ -21,19 +21,12 @@ st.markdown("""
 ##### Example questions the Virtual Hospital can help with:
             
 The Virtual Hospital can help with questions related to the flow and pathway of 
-**admitted**, **non-elective**, **adult** patients.
+**admitted**, **non-elective**, **adult** patients. This can help provide evidence for particular management strategies.
 
-* Given x beds, how far does admitted length of stay have to reduce to meet 
-particular waiting time targets for those queuing in ED? (Evidence based target)
-* If we open 15 beds but keep admitted length of stay the same, what is the 
-impact on ED waiting times and the various targets? (Evidence for a particular 
-management strategy)
-* What is the optimum number of people to stream from ED to SDEC to minimise ED
-waits for admission? (Evidence for a particular management strategy)
-* How does increase demand impact ED waiting times if the bed base stays static? 
-(Evidence for planning)
-            
-For some worked examples see the Examples page (under construction)
+Given a starting scenario where x daily 12hr breaches are occurring, what happens to breach numbers:
+* If we open x beds but keep admitted length of stay and demand the same?
+* If we open x beds and reduce inpatient length of stay?
+* If daily demand increases by x and beds and inpatient length of stay remain constant
 
 """)
 
@@ -51,8 +44,8 @@ of the 3 routes.
 The <span style='color:#FF1493; font-weight:bold'> pink arrow </span>
 represents the queue we are most interested in - these are the patients 
 waiting in ED who are admitted.
-* If a patient is waiting in ED for longer than the amount of time they were 
-expected to stay in hospital (thereby receiving their treatment in ED) they are discharged from ED.
+* The longer a patient is waiting in ED, the more likely they are to be discharged from ED rather than admitted
+(reneging).
 * When admitted to a bed a patient remains in that bed and the bed is unavailable
 until they are discharged.
 * The timepoints associated with each simulated patient (Decision To Admit,
@@ -68,10 +61,11 @@ st.markdown("""
             
 *"All models are wrong, some are useful" George Box*
 
-The Virtual Hospital is a simplified version of a real hospital, although some more
-complexity may be built in in the future. It may differ 
+The Virtual Hospital is a very simplified version of a real hospital, although some more
+complexity may be built in in the future. It differs
 from a real hospital in the following important ways. In the Virtual Hospital:
 
+* Beds are the only constraint 
 * There is no daily or weekly trend in when patients arrive.
 * ED, SDEC and other routes are assumed to be open at all times.
 * Patients can be discharged at any time of day/week.
@@ -81,13 +75,14 @@ from a real hospital in the following important ways. In the Virtual Hospital:
             
 This means that flow in the Virtual Hospital will always be **better** than flow in
 the real system (queue times in ED will be worse in the real system than in the 
-Virtual Hospital). 
+Virtual Hospital with the same inputs). 
 
 However, the Virtual Hospital can therefore be used to provide a
 best case scenario - if x beds, x demand and x los leads to 10 DTA breaches per day in the Virtual Hospital,
 you would expect **at least** this many DTA breaches per day in the  real system with the
-same parameters. And it can
-still be used to help understand the relationships between beds, demand and los.
+same parameters. It can also
+be used to help understand the relationships between beds, demand and los and provide evidence about the 
+required bedbase in the absence of other constraints.
             
 For more detail on model assumptions see the 'More Information' page
 """, unsafe_allow_html=True)
